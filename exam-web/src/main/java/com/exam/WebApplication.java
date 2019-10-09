f0caa206ed2f6a31c08b7352d869cd6b7f3b95e1
dtid_5ed5bf1df6f61cba <1530488472@qq.com>
2019-10-09 22:11:10 +0800

    commit all project 4

--- exam-web/src/main/java/com/exam/WebApplication.java
+++ exam-web/src/main/java/com/exam/WebApplication.java
@@ -15,6 +15,6 @@ public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
-        System.out.println("成功启动");
+        System.out.println("成功启动1");
     }
 }
