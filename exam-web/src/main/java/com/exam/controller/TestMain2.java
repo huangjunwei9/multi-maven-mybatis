801afb4c31f1cc5dbb860a3573fcd45ace496f94
dtid_5ed5bf1df6f61cba <1530488472@qq.com>
2019-10-10 14:57:14 +0800

    create 2 class files of TestMain, third

--- exam-web/src/main/java/com/exam/controller/TestMain2.java
+++ exam-web/src/main/java/com/exam/controller/TestMain2.java
@@ -7,6 +7,6 @@ package com.exam.controller;
  */
 public class TestMain2 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world1");
     }
 }
