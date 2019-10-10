d2238b45c840d07a3e9e21d719bf5b42b36e42db
dtid_5ed5bf1df6f61cba <1530488472@qq.com>
2019-10-10 22:28:26 +0800

    commit 前后端 sub-dev

--- exam-web/src/main/java/com/exam/controller/TestMain2.java
+++ exam-web/src/main/java/com/exam/controller/TestMain2.java
@@ -15,6 +15,6 @@ public class TestMain2 {
         System.out.println("Hello world 4");
         System.out.println("Hello world456");
 
-        System.out.println("合并：后端+前端（dev） ");
+        System.out.println("合并：后端+前端（sub-dev） ");
     }
 }
