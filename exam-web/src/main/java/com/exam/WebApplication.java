9a6d8fb50650e7466c5f68160a91d101eb268ebe
dtid_5ed5bf1df6f61cba <1530488472@qq.com>
2019-10-10 11:09:48 +0800

    10.10 11:09

--- exam-web/src/main/java/com/exam/WebApplication.java
+++ exam-web/src/main/java/com/exam/WebApplication.java
@@ -18,7 +18,7 @@ public class WebApplication {
         System.out.println("成功启动2");
         System.out.println("成功启动3   ?   啊");
         System.out.println("成功启动4   ?");
-        System.out.println("成功启动5   ?早上2019/10/10");
+        System.out.println("成功启动5   ?早上2019/10/10   你好");
     }
 }
 /** java招聘
