0db707d3b33fe46f3d83dac0c47d65abab925ad6
dtid_5ed5bf1df6f61cba <1530488472@qq.com>
2019-10-10 15:56:51 +0800

    add 后端

--- exam-web/src/main/java/com/exam/controller/TestMain2.java
+++ exam-web/src/main/java/com/exam/controller/TestMain2.java
@@ -8,5 +8,6 @@ package com.exam.controller;
 public class TestMain2 {
     public static void main(String[] args) {
         System.out.println("Hello world 2");
+        System.out.println("后端");
     }
 }
